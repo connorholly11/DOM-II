a6925a2f3eeed430cfd11dcb8e511e3c0253e8a3
Connor Holly <connorholly@Connors-MacBook-Pro-2.local>
2019-08-06 16:52:33 -0400

    3 done, 3 in progress

--- js/index.js
+++ js/index.js
@@ -1,1 +1,37 @@
 // Your code goes here
+
+// mouseover
+//  keydown
+//  wheel
+//  drag / drop
+//  load
+//  focus
+//  resize
+//  scroll
+//  select
+//  dblclick
+
+const logo = document.querySelector('.logo-heading');
+logo.addEventListener('click', function(e) {
+    console.log("You clicked me!")
+})
+
+const mouseover = document.querySelector('.home .intro h2');
+mouseover.addEventListener('mouseover', function(e){
+    document.body.style.backgroundColor = "red";
+})
+
+//got to work on this
+const drag = document.querySelector('.home .intro img');
+drag.addEventListener('drag', function(e){
+    event.dataTransfer.setData("img", event.target.id);
+})
+
+//got to work on this
+const dblclick = document.querySelectorAll('.container .content-section .img-content')[0];
+dblclick.addEventListener('dbleclick', function(e){
+    document.body.style.backgroundColor = "blue";
+});
+
+
+// const select = document.querySelector()
